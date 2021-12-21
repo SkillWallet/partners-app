82aa2ad2bc478e5676d0f6c192065f0f012f7810
taulant.disha <taulantdisha@Taulants-MacBook-Pro.local>
2021-12-21 23:51:26 +0200

    Minor fixes

--- config-overrides.js
+++ config-overrides.js
@@ -1,21 +1,24 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
-const { override, addWebpackAlias } = require('customize-cra');
+const { alias } = require('react-app-rewire-alias');
 
-module.exports = override(
-  addWebpackAlias({
-    '@assets': path.resolve(__dirname, './src/assets'),
-    '@auth': path.resolve(__dirname, './src/auth'),
-    '@api': path.resolve(__dirname, './src/api'),
-    '@utils': path.resolve(__dirname, './src/utils'),
-    '@store': path.resolve(__dirname, './src/redux'),
-    '@components': path.resolve(__dirname, './src/components'),
-    react: path.resolve('./node_modules/react'),
-    'react-dom': path.resolve('./node_modules/react-dom'),
-    '@mui/material': path.resolve('./node_modules/@mui/material'),
-    '@mui/icons-material': path.resolve('./node_modules/@mui/icons-material'),
-    '@emotion/react': path.resolve('./node_modules/@emotion/react'),
-    '@emotion/styled': path.resolve('./node_modules/@emotion/styled'),
-    'react-router-dom': path.resolve('./node_modules/react-router-dom'),
-  })
-);
+module.exports = {
+  webpack: (configuration) => {
+    return alias({
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@auth': path.resolve(__dirname, './src/auth'),
+      '@api': path.resolve(__dirname, './src/api'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@store': path.resolve(__dirname, './src/redux'),
+      '@components': path.resolve(__dirname, './src/components'),
+      react: path.resolve('./node_modules/react'),
+      'react-dom': path.resolve('./node_modules/react-dom'),
+      '@mui/material': path.resolve('./node_modules/@mui/material'),
+      '@mui/icons-material': path.resolve('./node_modules/@mui/icons-material'),
+      '@emotion/react': path.resolve('./node_modules/@emotion/react'),
+      '@emotion/styled': path.resolve('./node_modules/@emotion/styled'),
+      'react-router-dom': path.resolve('./node_modules/react-router-dom'),
+    })(configuration);
+  },
+};
