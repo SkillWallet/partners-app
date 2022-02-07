6a238a079fee936494610a18423f2b6414a70ce9
Milena Monova <mimonova13@gmail.com>
2022-02-07 13:49:05 +0200

    using latest webcompoennet version

--- src/api/textile.api.ts
+++ src/api/textile.api.ts
@@ -3,9 +3,11 @@ import { NFTStorage } from 'nft.storage';
 const client = new NFTStorage({ token: process.env.REACT_APP_NFT_STORAGE_KEY });
 
 export function ipfsCIDToHttpUrl(url: string, isJson: boolean) {
-  return isJson ?
-    `https://ipfs.io/ipfs/${url.replace('ipfs://', '')}/metadata.json` :
-    `https://ipfs.io/ipfs/${url.replace('ipfs://', '')}`
+  if (!url.includes('textile'))
+    return isJson
+      ? `https://ipfs.io/ipfs/${url.replace('ipfs://', '')}/metadata.json`
+      : `https://ipfs.io/ipfs/${url.replace('ipfs://', '')}`;
+  return url;
 }
 
 export async function storeMetadata(json) {
