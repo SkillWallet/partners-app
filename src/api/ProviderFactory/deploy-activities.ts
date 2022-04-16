f0282fc9863efcf68595c2abd49283f0f4574ce5
taulant.disha <taulantdisha@Taulants-MacBook-Pro.local>
2022-04-16 17:09:21 +0300

    Old roles fix

--- src/api/ProviderFactory/deploy-activities.ts
+++ src/api/ProviderFactory/deploy-activities.ts
@@ -2,6 +2,7 @@ import { ActivitiesABI, ActivitiesByteCode } from '@skill-wallet/sw-abi-types';
 import { ethers } from 'ethers';
 import { EnableAndChangeNetwork } from './web3.network';
 
+console.log(ActivitiesABI, 'ActivitiesABI');
 export const deployActivities = async (communityAddress: string, discordBotAddress: string) => {
   await EnableAndChangeNetwork();
   const webProvider = new ethers.providers.Web3Provider(window.ethereum);
